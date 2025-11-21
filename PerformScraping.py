2b678035d76fc0789d8e8c05af65d54ddadac629
Jayce <jstephe3@xula.edu>
2025-11-21 01:48:52 -0600

    Create function that returns drop dates for each semester

--- PerformScraping.py
+++ PerformScraping.py
@@ -12,10 +12,41 @@ class Scraper:
         soup = BeautifulSoup(response.text, "html.parser")
 
         if response.status_code == 200:
-            print("Fetched webpage")
-            print (soup)
+            return soup
         else:
             print("Could not find page")
 
-   
+    def getDropDates(soup):
+            target_text = "Last Day to Drop a Course"
+            date = ""
+            dropList = []
+            fallMonths = ["September", "October", "November", "December"]
+            springMonths = ["January", "February", "March", "April"]
+            summerMonths = ["May", "June", "July", "August"]
+            date_map = { }
+
+            titles = soup.find_all("tr")
+            for row in titles:
+                cells = row.find_all("td")
+                if cells:
+                    first_col = cells[0].text.strip().lower()
+                    if(target_text.lower() in first_col):
+                        date = cells[1].text.strip()
+                        if date:
+                            dropList.append(date)
+            
+            for c in dropList:
+                month = c.split()[0]
+                if(month in fallMonths):
+                    date_map.setdefault("Fall Semester",[]).append(c)
+                elif(month in springMonths):
+                    date_map.setdefault("Spring Semester",[]).append(c)
+                elif(month in summerMonths):
+                    date_map.setdefault("Summer Summester",[]).append(c)
+                 
+            
+            return date_map
+
+    
+
         
--- driver.py
+++ driver.py
@@ -2,7 +2,10 @@ from PerformScraping import Scraper
 class Driver:
 
     def main():
-        Scraper.getAcademicCalander()
+        select_soup = Scraper.getAcademicCalander()
+        semesterDropDates = Scraper.getDropDates(select_soup)
+
+        print(semesterDropDates)
     
     if __name__ == "__main__":
         main()
